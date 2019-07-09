e35b320403e991742892d0784581427fdde19d70
Wout Slakhorst <wout.slakhorst@nedap.com>
2019-07-09 13:30:25 +0200

    change logger to logrus in encryption step
    Signed-off-by: Wout Slakhorst <wout.slakhorst@nedap.com>

--- pkg/step-encrypt-fhir-consent.go
+++ pkg/step-encrypt-fhir-consent.go
@@ -20,10 +20,10 @@ package pkg
 
 import (
 	"fmt"
-	"github.com/labstack/gommon/log"
 	crypto "github.com/nuts-foundation/nuts-crypto/pkg"
 	cryptoTypes "github.com/nuts-foundation/nuts-crypto/pkg/types"
 	registry "github.com/nuts-foundation/nuts-registry/pkg"
+	"github.com/sirupsen/logrus"
 )
 
 func EncryptFhirConsent(registryClient registry.RegistryClient, cryptoClient crypto.Client, fhirConsent string, request CreateConsentRequest) (cryptoTypes.DoubleEncryptedCipherText, error) {
@@ -35,14 +35,14 @@ func EncryptFhirConsent(registryClient registry.RegistryClient, cryptoClient cry
 		// get public key for actor
 		organization, err := registryClient.OrganizationById(string(actor))
 		if err != nil {
-			log.Errorf("error while getting public key for actor: %v from registry: %v", actor, err)
+			logrus.Errorf("error while getting public key for actor: %v from registry: %v", actor, err)
 			return cryptoTypes.DoubleEncryptedCipherText{}, err
 		}
 		if organization.PublicKey == nil {
 			return cryptoTypes.DoubleEncryptedCipherText{}, fmt.Errorf("registry entry for organization %v does not contain a public key", actor)
 		}
 		pk := *organization.PublicKey
-		log.Debug("pk:", pk)
+		logrus.Debug("pk:", pk)
 		partyKeys = append(partyKeys, pk)
 	}
 	return cryptoClient.EncryptKeyAndPlainTextWith([]byte(fhirConsent), partyKeys)
